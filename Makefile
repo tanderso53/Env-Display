87741d3c7d6e9d1225be2b8400b209955eac6c08
Tyler Anderson <tasonchou@gmail.com>
2021-12-04 21:24:53 -0600

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,120 @@
+#####################################################################
+#                                                                   #
+#                            FILM MANAGER                           #
+#                  A program for logging photographic               #
+#                            film records                           #
+#                                                                   #
+#####################################################################
+
+# Copyright 2021 Tyler J. Anderson
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# film-manager Makefile
+
+srcdir		=	./src
+OBJDIR		:=	./objdir
+
+# BUILD SECTION
+CC		=	clang
+CXX		=	clang++
+VC		=	git
+CFLAGS		=	-Wall -g -I/usr/local/include
+CXXFLAGS	=	-std=c++17
+LDLIBS		=	-ljsoncpp -lncurses -lform -lc
+LDFLAGS		=	-L/usr/local/lib
+APP		=	env-display
+C_SRCS		=	main.c
+CXX_SRCS	=	jsonparse.cpp
+C_OBJS		=	$(addprefix $(OBJDIR)/,$(C_SRCS:.c=.o))
+CXX_OBJS	=	$(addprefix $(OBJDIR)/,$(CXX_SRCS:.cpp=.o))
+OBJS		:=	$(C_OBJS) $(CXX_OBJS)
+INSTROBJ	:=	$(OBJS:.o=.oi)
+H		=	jsonparse.h
+LICENSE		=	./LICENSE
+
+ifneq ("$(shell ls -a . | grep -c .git)", 0)
+CFLAGS		+=	-DFM_VERSION="\"$(shell $(VC) describe --long)\""
+endif
+
+.PHONY: all clean install coverage
+
+$(OBJDIR)/%.o: $(srcdir)/%.c $(addprefix $(srcdir)/,$(H))
+	@echo "*** BUILDING $@ ***"
+	$(CC) -c ${CFLAGS} -o $@ $<
+
+$(OBJDIR)/%.oi: $(srcdir)/%.c $(addprefix $(srcdir)/,$(H))
+	@echo "*** BUILDING $@ ***"
+	$(CC) -c ${CFLAGS} -fprofile-instr-generate -fcoverage-mapping \
+		-o $@ $<
+
+$(OBJDIR)/%.o: $(srcdir)/%.cpp $(addprefix $(srcdir)/,$(H))
+	@echo "*** BUILDING $@ ***"
+	$(CXX) -c ${CFLAGS} ${CXXFLAGS} -o $@ $<
+
+$(OBJDIR)/%.oi: $(srcdir)/%.cpp $(addprefix $(srcdir)/,$(H))
+	@echo "*** BUILDING $@ ***"
+	$(CXX) -c ${CFLAGS} ${CXXFLAGS} -fprofile-instr-generate \
+		-fcoverage-mapping -o $@ $<
+
+$(APP): $(OBJS)
+	@echo "*** BUILDING $@ ***"
+	$(CXX) ${CFLAGS} ${LDFLAGS} ${LDLIBS} -o $@ $(OBJS)
+	@echo "Complete! Install with \"make install\""
+
+all: $(APP)
+
+clean:
+	$(RM) $(APP)
+	$(RM) -R $(OBJDIR)
+
+coverage: $(INSTROBJ)
+	@echo "*** BUILDING $@ ***"
+	$(CXX) ${CFLAGS} ${LDFLAGS} ${LDLIBS} \
+		-fprofile-instr-generate -fcoverage-mapping -o $@ $(OBJS)
+
+$(OBJS): | $(OBJDIR)
+
+$(OBJDIR):
+	mkdir $(OBJDIR)
+
+# INSTALL SECTION
+INSTALL=install
+INSTALL_PROGRAM=$(INSTALL)
+INSTALL_DATA=$(INSTALL) -m 644
+PREFIX=/usr/local
+BINDIR=$(PREFIX)/bin
+DATAROOTDIR=$(PREFIX)/share
+DATADIR=$(DATAROOTDIR)
+SYSCONFDIR=$(PREFIX)/etc
+DOCDIR=$(DATAROOTDIR)/doc/$(APP)
+
+install: all
+	$(INSTALL_PROGRAM) $(APP) $(DESTDIR)$(BINDIR)/$(APP)
+	$(INSTALL_DATA) $(LICENSE) $(DESTDIR)$(DATADIR)/$(APP)/LICENSE
---
+++ src/main.c
@@ -0,0 +1,253 @@
+#include "jsonparse.h"
+#include <form.h>
+#include <math.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <poll.h>
+#include <fcntl.h>
+
+#define PARSE_FILE "/dev/stdin"
+
+static FIELD** fields = NULL;
+static FIELD** names = NULL;
+static FIELD** values = NULL;
+static FIELD** units = NULL;
+static unsigned int nfields = 0;
+static FORM* form = NULL;
+static WINDOW* win_form = NULL;
+static WINDOW* win_main = NULL;
+
+void parseData(int pdfd)
+{
+	char buff[1024];
+
+	//for (int i = 0; i < 1023; ++i) {
+	int readresult = read(pdfd, buff, 1023);
+
+	// if (readresult == 0) {
+	//   buff[i + 1] = '\0';
+	//   break;
+	// }
+
+	if (readresult < 0) {
+		perror("Error reading file: ");
+		raise(SIGINT);
+	}
+
+	buff[readresult] = '\0';
+	//}
+
+	initializeData(buff);
+	struct datafield* df = NULL;
+	df = getDataDump(df);
+
+	int nlimit;
+
+	if ((unsigned int) numDataFields() < nfields)
+		nlimit = numDataFields();
+	else
+		nlimit = nfields;
+
+	for (int i = 0; i < nlimit; ++i) {
+		set_field_buffer(names[i], 0, df[i].name);
+		set_field_buffer(values[i], 0, df[i].value);
+		set_field_buffer(units[i], 0, df[i].unit);
+	}
+
+	clearData();
+}
+
+void popFields()
+{
+	// int wfd = open("/dev/stdout", O_RDWR);
+	// char msg[] = "hello";
+	// write(wfd, msg, strlen(msg));
+	// close(wfd);
+
+	struct pollfd pfd = {
+		.fd = open(PARSE_FILE, O_RDWR),
+		.events = POLLIN
+	};
+
+	int pollresult = poll(&pfd, 1, 60000);
+
+	if (pollresult < 0) {
+		perror("Error while reading initial data: ");
+		raise(SIGINT);
+	}
+
+	if (pollresult == 0) {
+		fprintf(fopen("/dev/stderr", "w"), "Failed to get "
+			"initial data from stream\n");
+		raise(SIGINT);
+	}
+
+	char buff[1024];
+	struct datafield* dfields = NULL;
+
+	//for (int i = 0; i < 1023; ++i) {
+	int readresult = read(pfd.fd, buff, 1023);
+
+	// if (readresult == 0) {
+	//   buff[i + 1] = '\0';
+	//   break;
+	// }
+
+	if (readresult < 0) {
+		perror("Error reading file: ");
+		raise(SIGINT);
+	}
+
+	buff[readresult] = '\0';
+
+	//buff[i] = cbuff;
+	//}
+
+	initializeData(buff);
+	nfields = numDataFields();
+	dfields = getDataDump(dfields);
+
+	assert(dfields);
+
+	names = (FIELD**) malloc(nfields * sizeof(FIELD*));
+	values = (FIELD**) malloc(nfields * sizeof(FIELD*));
+	units = (FIELD**) malloc(nfields * sizeof(FIELD*));
+	fields = (FIELD**) malloc((nfields * 3 + 1) * sizeof(FIELD*));
+
+	int fieldsctr = 0;
+
+	for (unsigned int i = 0; i < nfields; ++i) {
+		names[i] = new_field(1, 15, i * 2, 2, 0, 0);
+		values[i] = new_field(1, 15, i * 2, 17, 0, 0);
+		units[i] = new_field(1, 10, i * 2, 34, 0, 0);
+
+		set_field_buffer(names[i], 0, dfields[i].name);
+		set_field_buffer(values[i], 0, dfields[i].value);
+		set_field_buffer(units[i], 0, dfields[i].unit);
+
+		field_opts_off(names[i], O_ACTIVE);
+		field_opts_off(values[i], O_ACTIVE);
+		field_opts_off(units[i], O_ACTIVE);
+
+		set_field_just(values[i], JUSTIFY_RIGHT);
+
+		fields[fieldsctr] = names[i]; fieldsctr++;
+		fields[fieldsctr] = values[i]; fieldsctr++;
+		fields[fieldsctr] = units[i]; fieldsctr++;
+	}
+
+	fields[fieldsctr] = NULL;
+	clearData();
+	close(pfd.fd);
+}
+
+void formDriver(int ch)
+{
+	switch (ch) {
+
+	default:
+		break;
+
+	}
+}
+
+void formExit()
+{
+	if (form) {
+		unpost_form(form);
+		free_form(form);
+	}
+
+	if (fields) {
+		for (int i = 0; fields[i] != NULL; ++i) {
+			if (fields[i]) {
+				free_field(fields[i]);
+			}
+		}
+	}
+
+	endwin();
+}
+
+int formRun()
+{
+	initscr();
+	win_main = newwin(30, /* Lines */
+			  80, /* Cols */
+			  0, /* X */
+			  0); /* Y */
+	box(win_main, 0, 0);
+	win_form = derwin(win_main,
+			  26, /* Lines */
+			  76, /* Cols */
+			  2, /* X */
+			  2); /* Y */
+	box(win_form, 0, 0);
+
+	assert(win_main);
+	assert(win_form);
+
+	popFields();
+	form = new_form(fields);
+	assert(form);
+
+	if (set_form_win(form, win_main) != 0) {
+		perror("Critical Error setting main window: ");
+		return 1;
+	}
+
+	if (set_form_sub(form, derwin(win_form, 24, 74, 1, 1)) != 0) {
+		perror("Critical Error setting sub window: ");
+		return 1;
+	}
+
+	// Title Header
+	mvwprintw(win_main, 1, 2, "Environmental Data");
+
+	post_form(form);
+	refresh();
+	wrefresh(win_main);
+	wrefresh(win_form);
+
+	for (;;) {
+		struct pollfd pfd = {
+			.fd = open(PARSE_FILE, O_RDWR),
+			.events = POLLIN
+		};
+
+		int pollresult = poll(&pfd, 1, 30000);
+
+		if (pollresult < 0) {
+			perror("Critical Error polling file: ");
+			formExit();
+			exit(1);
+		}
+		else if (pollresult == 0) {
+			continue;
+		}
+
+		parseData(pfd.fd);
+		refresh();
+		wrefresh(win_main);
+		wrefresh(win_form);
+		close(pfd.fd);
+	}
+}
+
+void signalHandler(int sig)
+{
+	formExit();
+	printf("Received signal %d\n", sig);
+	exit(0);
+}
+
+int main()
+{
+	signal(SIGINT, signalHandler);
+	return formRun();
+}
